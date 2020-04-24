b9731f2d3e4945f25a831d05ba9ea85020b5a6c0
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-04-24 07:36:10 -0700

    More Learning, Updated Prospectable Tagline

--- scriptLearning.js
+++ scriptLearning.js
@@ -142,6 +142,7 @@ if (civilStatus === 'married') {
 * Boolean logic
 */
 
+/*
 var firstName = 'John';
 var age = 16;
 
@@ -151,6 +152,76 @@ if (age < 13) {
     console.log(firstName + ' is a teenager.')
 } else {
     console.log(firstName + ' is a man.');
+}
+*/
+
+/******************
+* The Ternary Operator and Switch Statements
+*/
+
+//ternary operator
+
+/*
+
+var firstName = 'John';
+var age = 20;
+
+age >= 18 ? console.log(firstName + ' drinks beer.')
+: console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+*/
+
+//Switch statement
+/*
+var job = 'teacher';
+switch (job) {
+    case 'teacher':
+            console.log(firstName + ' teaches kids how to code.');
+            break;
+    case 'driver':
+            console.log(firstName + ' drives an uber in Lisbon');
+            break;
+    case 'designer':
+            console.log(firstName + ' designs beautiful websites.');
+            break;
+    default:
+            console.log(firstName + ' does something else.');
+
+}
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + 'John is a man')
+}
+    
+*/
+
+/*********************************
+* Truthy and Falsy values and equality operators
+*/
+
+// falsy values: undefined, null, 0, ' ', NaN
+
+// truthy values: NOT falsy values
+
+var height;
+
+height = 0;
+
+if (height || height ===0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable has NOT been defined');
 }
 
 
