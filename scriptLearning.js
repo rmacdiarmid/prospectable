fde81dc6d5abea6d74a28140175426787dba44c4
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-04-24 07:58:22 -0700

    Domain ideas, and more learning

--- scriptLearning.js
+++ scriptLearning.js
@@ -216,14 +216,19 @@ switch (true) {
 
 var height;
 
-height = 0;
+height = 23;
 
-if (height || height ===0) {
+if (height || height === 0) {
     console.log('Variable is defined');
 } else {
     console.log('Variable has NOT been defined');
 }
 
+// Equality operators (data types do not need to matah)
 
+if (height == '23') {
+    console.log('The == operator does type coercion');
+    
+}
 
     
