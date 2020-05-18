33212b01f7dffa44a9ad536134b1f05473df9480
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-05-18 04:31:06 -0700

    move /static to public/static because of error code

--- frontendPROSPECT/.next/static/runtime/webpack.js
+++ frontendPROSPECT/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6be8f32dd532527d5634";
+/******/ 	var hotCurrentHash = "8b88b4708c508c8adc9e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontendPROSPECT/pages/_app.js
+++ frontendPROSPECT/pages/_app.js
@@ -1,18 +1,14 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Page from '../components/Page';
 
 class MyApp extends App {
     render() {
-        const { Component } = this.props;
-
-        return (
-            <Container>
-                <Page>    
-                <Component />
-                </Page>
-            </Container>
-        )
+      const { Component, pageProps } = this.props
+      return <Component {...pageProps} />
     }
-}
+  }
 
 export default MyApp;
+
+
+
