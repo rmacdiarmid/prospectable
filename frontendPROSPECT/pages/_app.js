eef008e547b1bcb6b114eebd87b315607215633d
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-05-18 05:25:39 -0700

    _app.js change back page not ruending app

--- frontendPROSPECT/pages/_app.js
+++ frontendPROSPECT/pages/_app.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import App from 'next/app';
+import App, { Container } from 'next/app';
 import Page from '../components/Page';
 
 class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />
-  }
-}
+    render() {
+        const { Component } = this.props;
+
+        return (
+            <Container>
+                <Page>    
+                <Component />
+                </Page>
+            </Container>
+        )
+    }
 
 export default MyApp;
