12b3d4c6962ed9199be8897f5a036e01cbd28bff
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-06-14 06:46:54 -0700

    Lesson 12 - 21:00 - Link in title on Item.js

--- frontendprospect/components/Item.js
+++ frontendprospect/components/Item.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
@@ -10,7 +11,15 @@ class Item extends Component {
         const { item } = this.props;
         return (
             <ItemStyles>
-                <Title>{item.title}</Title>
+                <Title>
+                  <Link href={{
+                      pathname: '/item',
+                      query: { id: item.id },
+                    }}
+                  >
+                    <a>{item.title}</a>
+                  </Link>
+                </Title>
             </ItemStyles>
         );
     }
