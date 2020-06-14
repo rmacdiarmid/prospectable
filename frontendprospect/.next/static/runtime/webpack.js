5740db88ddc41be3ac1edc853b39a00b4fb51dcc
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-06-14 05:36:17 -0700

    Lesson 12 - 12:09 - Ran first query... it worked!

--- frontendprospect/.next/static/runtime/webpack.js
+++ frontendprospect/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e67b60358bda2a00f2ac";
+/******/ 	var hotCurrentHash = "0b7b0804c39ae13933d8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontendprospect/components/Items.js
+++ frontendprospect/components/Items.js
@@ -22,9 +22,10 @@ class Items extends Component {
             <div>
                 <p>Items!</p>
                 <Query query={ALL_ITEMS_QUERY}>
-                    {(payload) => {
-                        console.log(payload);
-                        return <p>Hey I'm the child of query</p>;
+                    {({ data, error, loading }) => {
+                        if (loading) return <p>Loading...</p>;
+                        if (error) return <p>Error: {error.message}</p>;
+                        return <p>I found {data.items.length} items!</p>;
                     }}
                 </Query>
             </div>
