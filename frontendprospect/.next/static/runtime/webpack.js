d26bb67083c0bdf5523244e5ea9d76abc3f20a4a
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-06-18 22:58:18 -0700

    Deleted items and created items

--- frontendprospect/.next/static/runtime/webpack.js
+++ frontendprospect/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "74d95cdb8f6cb9ebf145";
+/******/ 	var hotCurrentHash = "7a3f2cf09a4e87333bf4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
