f2a861e02dee8e83591679b93363041d14bf9c66
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-06-19 05:01:20 -0700

    Lesson 22 - 5:36 - Got pagination markers, backend itemConnection setup

--- frontendprospect/.next/static/runtime/webpack.js
+++ frontendprospect/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7a3f2cf09a4e87333bf4";
+/******/ 	var hotCurrentHash = "b1509cc330161e6f0f8b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontendprospect/components/Pagination.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+import gql from 'graphql-tools';
+
+const Pagination = props => (
+    <PaginationStyles>
+        <p>Hi I'm the Pagination</p>
+    </PaginationStyles>
+)
+
+export default Pagination;
