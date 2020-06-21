f513765a96ff570f802854cbb32819b6bba0f775
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-06-21 06:11:49 -0700

    LESSON 29 - 10:21 - Set Mutation on backend

--- backendPROSPECT/src/resolvers/Mutation.js
+++ backendPROSPECT/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
@@ -91,7 +93,27 @@ const Mutations = {
     signout(parent, args, ctx, info) {
         ctx.response.clearCookie('token');
         return { message: 'Goodbye'};
-    }
+    },
+    async requestReset(parent, args, ctx, info) {
+        // 1. Check if this is a real user
+        const user = await ctx.db.query.user({ where: { email: args.email } })
+        ;
+        if(!user) {
+            throw new Error(`No such user found for email ${args.email}`);
+        }
+        // 2. Set a reset token and expiry on that user
+        const randomBytesPromiseified = promisify(randomBytes);
+        const resetToken = (await randomBytesPromiseified(20)).toString('hex');
+        const resetTokenExpiry = Date.now() + 3600000; //1 hour from now
+        const res = await ctx.db.mutation.updateUser({
+            where: { email: args.email },
+            data: { resetToken, resetTokenExpiry },
+        });
+        console.log(res);
+        return { message: `Thanks!`};
+        // 3. Email them that reset token
+        
+    },
 };
 
 module.exports = Mutations;
