e43703484412ae7edd31a32df6b0d2ad975a35ec
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2020-07-07 19:47:09 -0700

    Commiting generated files mostly before "forking" to new database for prospectable

--- frontendprospect/.next/static/runtime/webpack.js
+++ frontendprospect/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7440869acd59e0c86890";
+/******/ 	var hotCurrentHash = "a8785aa959bd754fddb5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontendprospect/components/Permissions.js
+++ frontendprospect/components/Permissions.js
@@ -14,6 +14,18 @@ const possiblePermissions = [
     'PERMISSIONPUPDATE',
 ];
 
+const UPDAGE_PERMISSION_MUTATION = gql`
+    mutation updatePermissions($permissions: [Permission],
+    $userId: ID!) {
+        updatedPermissions(permissions: $permissions, userId: $userId) {
+            id
+            permissions
+            name
+            email
+        }
+    }
+    `;
+
 const ALL_USERS_QUERY = gql`
     query {
         users {
@@ -44,7 +56,7 @@ const Permissions = (props) => (
                           </tr>
                       </thead>
                       <tbody>
-                      {data.users.map(user => <UserPermissions user={user} key={user.id} />)}
+                        {data.users.map(user => <UserPermissions user={user} key={user.id} />)}
                       </tbody>
                   </Table>
               </div>
@@ -83,23 +95,40 @@ class UserPermissions extends React.Component {
     render() {
         const user = this.props.user;
         return (
-            <tr>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                {possiblePermissions.map(permission => (
-                    <td key={permission}>
-                        <label htmlFor={`${user.id}-permission-${permission}`}>
-                            <input type="checkbox" checked={this.state.permissions.includes(permission)} 
-                                value={permission}
-                                onChange={this.handlePermissionChange}
-                            />
-                        </label>
-                    </td>
-                ))}
-                <td>
-                    <SickButton>Update</SickButton>
-                </td>
-            </tr>
+            <Mutation mutation={UPDATE_PERMISSIONS_MUTATION} variables={{
+                permissions: this.state.permissions,
+                userId: this.props.user.id
+              }}
+            >
+            {(updatePermissions, { loading, error }) => (
+               <> 
+                <tr>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    {possiblePermissions.map(permission => (
+                        <td key={permission}>
+                            <label htmlFor={`${user.id}-permission-${permission}`}>
+                                <input 
+                                    id={`${user.id}-permission-${permission}`}
+                                    type="checkbox" 
+                                    checked={this.state.permissions.includes(permission)} 
+                                    value={permission}
+                                    onChange={this.handlePermissionChange}
+                                />
+                            </label>
+                        </td>
+                    ))}
+                    <td>
+                    <SickButton 
+                        type="button"
+                        disabled={loading}
+                        onClick={updatePermission}>
+                    >Update</SickButton>
+                 </td>
+                </tr>
+                </>
+            )}
+            </Mutation>
         );
     }
 }
